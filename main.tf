9b8e27a902e16b15e3e867c9a14183a93ee576ea
shashanksingh chauhan <shashankchauhan1591@gmail.com>
2024-08-05 12:31:07 +0530

    my first commit 05-08

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources9876"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example9876"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "example" {
+  name                = "example9876"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
---
+++ variable.tf
@@ -0,0 +1,29 @@
+# variable "resource_group_name" {
+#   description = "The name of the resource group"
+#   type        = string
+#   default     = "myResourceGroup"
+# }
+
+# variable "location" {
+#   description = "The Azure location where the resources will be created"
+#   type        = string
+#   default     = "West Europe"
+# }
+
+# variable "app_service_plan_name" {
+#   description = "The name of the App Service Plan"
+#   type        = string
+#   default     = "myAppServicePlan"
+# }
+
+# variable "web_app_name" {
+#   description = "The name of the Web App"
+#   type        = string
+#   default     = "myWebApp"
+# }
+
+# variable "app_service_sku" {
+#   description = "The SKU of the App Service Plan"
+#   type        = string
+#   default     = "F1"
+# }
